a1a7e017fb22a6605c48452f9830453f404d716e
sandesh mankar <sandeshtnk11@gmail.com>
2020-02-05 18:02:34 +0800

    create new controller for stock

--- index.js
+++ index.js
@@ -7,54 +7,17 @@ const {successResponse, errorHandler,notFoundError,badRequestError} = require('.
 const mio = require('./socket');
 app.use(bodyParser.urlencoded({extended: true}));
 
+const { getStock } = require('./controller/sh000001');
+
 let stock ={}; 
 
 
-app.get('/home', function (req,res) {
-    return res.send(successResponse(stock,"sh000001"));
+app.get('/home', function async (req,res) {
+    const data = await getStock();
+    return res.send(successResponse(data,"sh000001"));
 });
 
-async function stockData() { 
-        
-    let stockUrl = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sh000001&scale=5&ma=no&datalen=140";
 
-    requestPromise(stockUrl, {json: true })
-    .then(function(res){      
-        
-        // replace all string object
-        var data = res.replace(/{day:/g,'{"day":').replace(/,open:/g,',"open":').replace(/,high:/g,',"high":').replace(/,low:/g,',"low":').replace(/,close:/g,',"close":').replace(/,volume:/g,',"volume":');
-
-        arrayData = [];     
-        
-        // Sting convert to JSON
-        let finalStock = JSON.parse(data); 
-        // Count Json Object Data
-        console.log(typeof finalStock);
-        var count= Object.keys(finalStock).length;
-        
-        // For Loop for create new Response
-        for (i = 0; i < count; i++) {
-            var obj = {};
-            const open = finalStock[i].open;            
-            obj['date'] = finalStock[i].day;
-            obj['no1']  = open.charAt(open.length-2);  
-            obj['no2']  = open.charAt(open.length-1);       
-            obj['PT'] = open;
-
-            // all object data will store in arrayData 
-            arrayData.push(obj);           
-        }
-        // array will be reverse and latest data will be first
-        stockReverse = arrayData.reverse();    
-        // stockReverse array store in stock data array    
-        stock = stockReverse;  
-       
-    })
-    .catch(function(err) {
-        // Crawling failed...
-        console.log(err)
-    });   
-}
 
 const server = app.listen(port, () => {
     stockData();
